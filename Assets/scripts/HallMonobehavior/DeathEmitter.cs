960a7513f84f805223a15efb7a03f4c954c1766f
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2018-04-27 08:28:51 +0800

    Implement shootable behaviour for enemies

---
+++ Assets/scripts/HallMonobehavior/DeathEmitter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathEmitter : MonoBehaviour {
+    private List<DeathListener> listeners;
+
+    void Awake()
+    {
+        listeners = new List<DeathListener>();
+    }
+
+    public void Subscribe (DeathListener d)
+    {
+        listeners.Add(d);
+    }
+
+    public void EmitDeath()
+    {
+        foreach (DeathListener d in listeners)
+        {
+            d.Notify(this);
+        }
+    }
+}
---
+++ Assets/scripts/HallMonobehavior/EnemyShootable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootable : Shootable {
+    [SerializeField]
+    private int baseHealth;
+
+    private int currentHealth;
+    private DeathEmitter emitter;
+
+    void Awake () {
+        currentHealth = baseHealth;
+        emitter = GetComponent<DeathEmitter>();
+	}
+    
+    public override void TakeDamage()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            if (emitter != null)
+            {
+                emitter.EmitDeath();
+            }
+
+            Despawn();
+        }
+    }
+
+    void Despawn()
+    {
+        DestroyObject(gameObject);
+    }
+}
